377497e216ef2655de85148ad0e4e491b5ca6a00
Paul Ralyea <047par@gmail.com>
2020-03-10 04:09:46 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -27,9 +27,8 @@ end
 
 def aoh_lookup(aoh, row, key)
   aoh = [{ name:  "Don Gately", occupation: "Live-in Staff" }, { name:  "Joelle van Dyne", occupation: "Radio Personality" }, { name:  "Pat Monteseian", occupation: "Staff" }, { name:  "Kate Gompert", occupation: "None" }, { name:  "Bruce Green", occupation: "Fan of Mildred" }],
-lookup = aoh[row][key],
-lookup
-  
+
+aoh[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
